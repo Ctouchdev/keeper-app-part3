862800394666b890ef3570be73a815891b92ce7e
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-22 09:28:39 -0500

    Initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function onSubmit(e) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+
+    e.preventDefault();
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          value={note.title}
+          onChange={handleChange}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          value={note.content}
+          onChange={handleChange}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={onSubmit}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
